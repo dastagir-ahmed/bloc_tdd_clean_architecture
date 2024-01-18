f52626059ce9acd3e0f8858f371fbb9f002816f7
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-01-18 14:42:00 +0800

    added expect result

--- test/src/authentication/domain/usecases/create_user_test.dart
+++ test/src/authentication/domain/usecases/create_user_test.dart
@@ -1,6 +1,7 @@
 //what does this class depend on?
 //How can we create a fake version of the dependency
 //How do we controll what are denendecies
+import 'package:bloc_tdd/core/errors/failure.dart';
 import 'package:bloc_tdd/src/authentication/domain/repositories/authentication_repository.dart';
 import 'package:bloc_tdd/src/authentication/domain/usecases/create_user.dart';
 import 'package:dartz/dartz.dart';
@@ -24,7 +25,8 @@ void main() {
         repository.createUser(name: any(named:'name' ), avatar: any(named:'avatar'), createdAt: any(named:'createdAt')))
         .thenAnswer((_) async => const Right(null));
     //act
-    usecase(params);
+    final result = await usecase(params);
     //assert
+    expect(result, equals(const Right<Failure, void>(null)));
   });
 }
